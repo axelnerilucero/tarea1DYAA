826836f14afa8d5ee5947c1d37994b587aaebb16
axelnerilucero <neriluceroaxel@gmail.com>
2021-08-31 17:59:41 -0500

    pequeño cambio

--- tarea1.py
+++ tarea1.py
@@ -1,11 +1,11 @@
 class NodoArbol:
-    def __init__( self, value, left=None, mid=None, right=None ):
+    def __init__( self, value, left=None, midl=None, right=None ):
         self.data = value
         self.left = left
-        self.mid = mid
+        self.midl = midl
         self.right = right
 
 head = NodoArbol(20, None, NodoArbol(19, NodoArbol(67, None, NodoArbol(99, None, None, None), None), None, None), NodoArbol(23, None, NodoArbol(57, None, None, ), None))
 
-print(head.mid.left.mid.data)
-print(head.right.mid.data)
+print(head.midl.left.midl.data)
+print(head.right.midl.data)
